df11478f5a46e1e6803978a55fc87527694f29c5
Quan Nguyen <sendquananemail@gmail.com>
2016-02-12 22:24:44 +0700

    refactor get oxford topic dictionary

--- oxfordtopic.php
+++ oxfordtopic.php
@@ -25,29 +25,9 @@ foreach($homePage->find('#topic-list dd dl dt') as $element){
 // get subcategory's url
 $subcategoryUrls = array();
 
-foreach ($homePage->find('#topic-list dd dl dd ul li a') as $element) {
-    $subcategoryUrls[] = $element->getAttribute('href');
+foreach ($homePage->find('#topic-list dd dl dd ul li a') as $subcategoryUrl) {
+    $subcategoryUrls[] = $subcategoryUrl->getAttribute('href');
 }
-//
-////get word's url
-////$first_subcategory_url = 'http://www.oxfordlearnersdictionaries.com/topic/animal_homes';
-////$document = new Document($first_subcategory_url, true);
-////
-////$word_urls = array();
-////
-////foreach ($document->find('.wordpool li a') as $element) {
-////    $word_urls[] = $element->getAttribute('href');
-////}
-//
-////get word's definition
-////$word_url = 'http://www.oxfordlearnersdictionaries.com/topic/animal_homes/aviary';
-////$document = new Document($word_url, true);
-////
-////$word_definitions = array();
-////
-////foreach ($document->find('.sn-gs span .def') as $element) {
-////    $word_definitions[] = $element->text();
-////}
 
 // Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
@@ -58,10 +38,12 @@ $objPHPExcel->getActiveSheet()->setCellValue('A1', "Entry");
 $objPHPExcel->getActiveSheet()->setCellValue('B1', "Subcategory");
 $objPHPExcel->getActiveSheet()->setCellValue('C1', "Category");
 $objPHPExcel->getActiveSheet()->setCellValue('D1', "Subject");
+$objPHPExcel->getActiveSheet()->setCellValue('E1', "Part of Speech");
+$objPHPExcel->getActiveSheet()->setCellValue('F1', "Definition");
 
 // Add data to first sheet
 $objPHPExcel->setActiveSheetIndex(0);
-$numberOfEntry = 2;
+$count = 2;
 foreach ($subcategoryUrls as $subcategoryUrl) {
     $document = new Document($subcategoryUrl, true);
 
@@ -69,26 +51,27 @@ foreach ($subcategoryUrls as $subcategoryUrl) {
     $categoryName = $categorySubcategoryNames[$subcategoryName];
     $subjectName = $document->find('.selected')[1]->text();
 
-//    $word_urls = array();
-//
-//    foreach ($document->find('.wordpool li a') as $element) {
-//        $word_urls[] = $element->getAttribute('href');
-//    }
-
-    foreach ($document->find('.wordpool li') as $entry) {
-        $objPHPExcel->getActiveSheet()->setCellValue('A' . $numberOfEntry, $entry->text())
-                                    ->setCellValue('B' . $numberOfEntry, $subcategoryName)
-                                    ->setCellValue('C' . $numberOfEntry, $categoryName)
-                                    ->setCellValue('D' . $numberOfEntry, $subjectName);
-        $numberOfEntry++;
+    $entryUrls = array();
+    foreach ($document->find('.wordpool li a') as $entryUrl) {
+        $entryUrls[] = $entryUrl->getAttribute('href');
     }
 
-//    foreach($word_urls as $word_url){
-//        $document = new Document($word_url, true);
-//
-//        foreach ($document->find('.sn-gs span .def') as $element) {
-//        }
-//    }
+    foreach($entryUrls as $entryUrl){
+        $document = new Document($entryUrl, true);
+
+        $entryName = $document->find('.h')[0]->text();
+        $partOfSpeech = $document->find('.pos')[0]->text();
+
+        foreach ($document->find('.sn-gs span .def') as $entryDefinition) {
+            $objPHPExcel->getActiveSheet()->setCellValue('A' . $count, trim($entryName))
+                ->setCellValue('B' . $count, $subcategoryName)
+                ->setCellValue('C' . $count, $categoryName)
+                ->setCellValue('D' . $count, $subjectName)
+                ->setCellValue('E' . $count, $partOfSpeech)
+                ->setCellValue('F' . $count, trim($entryDefinition->text()));
+            $count++;
+        }
+    }
 }
 
 // Rename first worksheet
@@ -133,7 +116,6 @@ $objPHPExcel->setActiveSheetIndex(1);
 
 // Save Excel 2007 file
 $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-$objWriter->setPreCalculateFormulas(false);
 $objWriter->save(str_replace('.php', '.xlsx', __FILE__));
 
 ?>

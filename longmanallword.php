350c146c66a863d343d16ea92c15625a602c82c2
Quan Nguyen <sendquananemail@gmail.com>
2016-03-01 17:13:43 +0700

    calculate entry html length

--- longmanallword.php
+++ longmanallword.php
@@ -17,6 +17,8 @@ $writer->addRow($headerRow);
 # Create a connection
 $url = 'http://global.longmandictionaries.com/dict_search/get_initial_entries/ldoce6/';
 
+$maxlength_other_words = 0;
+$maxlength_entry_for_alpha_key = 0;
 do {
     $ch = curl_init($url);
     # Setting options
@@ -38,10 +40,53 @@ do {
         $alphaKey = $element->getAttribute('data-alphakey');
         $key = $element->getAttribute('data-key');
 
+        # new data
+        $data = array(
+            'alpha_key' => $alphaKey,
+            'name' => ''
+        );
+        # Create a connection
+        $url = 'http://global.longmandictionaries.com/dict_search/entry_for_alpha_key/ldoce6/';
+        $ch = curl_init($url);
+        # Form data string
+        $postString = http_build_query($data, '', '&');
+        # Setting options
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        # Get the response
+        $entry_for_alpha_key = curl_exec($ch);
+        if($maxlength_entry_for_alpha_key < strlen($entry_for_alpha_key)){
+            $maxlength_entry_for_alpha_key = strlen($entry_for_alpha_key);
+        }
+        curl_close($ch);
+
+        # new data
+        $data = array(
+            'entry_key' => $key,
+        );
+        # Create a connection
+        $url = 'http://global.longmandictionaries.com/dict_search/other_words/ldoce6/';
+        $ch = curl_init($url);
+        # Form data string
+        $postString = http_build_query($data, '', '&');
+        # Setting options
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        # Get the response
+        $other_words = curl_exec($ch);
+        if($maxlength_other_words < strlen($other_words)){
+            $maxlength_other_words = strlen($other_words);
+        }
+        curl_close($ch);
+
         $singleRow = [$entryName, $pos, $alphaKey, $key, $element->html()];
         $writer->addRow($singleRow);
 
         echo $entryName . PHP_EOL;
+        echo $maxlength_entry_for_alpha_key . PHP_EOL;
+        echo $maxlength_other_words . PHP_EOL;
     }
 
     if ($alphaKey == 'insipidness,_insipidity_d1') {

d42414b572c4dbdb6d3e5636901291855b31ab23
Quan Nguyen <sendquananemail@gmail.com>
2016-03-03 02:36:27 +0700

    check if word is NOT in culture word list

--- longmanstatic.php
+++ longmanstatic.php
@@ -5,6 +5,7 @@ $startTime = microtime(true);
 require "vendor/autoload.php";
 
 use DiDom\Document;
+use DiDom\Query;
 use Box\Spout\Reader\ReaderFactory;
 use Box\Spout\Common\Type;
 
@@ -17,29 +18,34 @@ foreach ($reader->getSheetIterator() as $sheet) {
     foreach ($sheet->getRowIterator() as $row) {
 
         $document = new Document($row[5]);
-        if($document->has('.hyphenation')){
-            $word = $document->find('.hyphenation')[0];
-            echo 'Word: ' . $word->text() . PHP_EOL;
-        }
+        //check if word is NOT in culture word list
+        if (count($elements = $document->find("//span[contains(@type, 'encyc')]", Query::TYPE_XPATH)) == 0) {
 
-        foreach ($document->find('.sense') as $element) {
-            if ($element->has('.subsense')) {
-                foreach ($element->find('.subsense .def') as $item) {
-                    echo trim($item->text()) . PHP_EOL;
-                    $count++;
-                }
-            } else {
-                if ($element->has('.def')) {
-                    $item = $element->find('.def')[0];
-                    echo trim($item->text()) . PHP_EOL;
-                    $count++;
+            if ($document->has('.hyphenation')) {
+                $word = $document->find('.hyphenation')[0];
+                echo 'Word: ' . $word->text() . PHP_EOL;
+            }
+
+            foreach ($document->find('.sense') as $element) {
+                if ($element->has('.subsense')) {
+                    foreach ($element->find('.subsense .def') as $item) {
+                        echo trim($item->text()) . PHP_EOL;
+                        $count++;
+                    }
+                } else {
+                    if ($element->has('.def')) {
+                        $item = $element->find('.def')[0];
+                        echo trim($item->text()) . PHP_EOL;
+                        $count++;
+                    }
                 }
             }
+
         }
 
     }
-    echo $count . PHP_EOL;
 }
+echo 'Total definition: ' . $count . PHP_EOL;
 
 $reader->close();
 
